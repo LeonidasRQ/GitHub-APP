4b5168b5413f18015a15a76a502c44e1aeb61199
Leonidas <leonidas.ramirez.q@gmail.com>
2022-05-09 07:55:09 -0500

    commit model functionalities

--- server/src/models/commits/commits.model.js
+++ server/src/models/commits/commits.model.js
@@ -13,8 +13,60 @@ async function populateCommits() {
     "GET /repos/leonidasrq/GitHub-APP/commits"
   );
 
+  const commmits = response.data;
+
   if (response.status !== 200) {
     console.log("Problem downloading posts data");
     throw new Error("Post data download failed");
   }
+
+  for (const commit of commits) {
+    const newCommit = {
+      sha: commit["sha"],
+      authorName: commit["commit"]["author"]["name"],
+      authorEmail: commit["commit"]["author"]["email"],
+      creationDate: commit["commit"]["author"]["date"],
+      commitMessage: commit["commit"]["message"],
+    };
+
+    const commitExists = await exitsCommitWithSha(newCommit.sha);
+    if (!commitExists) {
+      console.log(
+        `New commit retreived - sha: ${newCommit.sha} creation date: ${newCommit.creationDate}`
+      );
+      await saveCommit(newCommit);
+    }
+  }
 }
+
+async function findCommit(filter) {
+  return await commitsDB.findOne(filter);
+}
+
+async function exitsCommitWithSha(sha) {
+  return await findCommit({
+    sha: sha,
+  });
+}
+
+async function getAllCommits(skip, limit) {
+  return await commitsDB
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ creationDate: 1 })
+    .skip(skip)
+    .limit(limit);
+}
+
+async function saveCommit(commit) {
+  await commitsDB.findOneAndUpdate(
+    {
+      objectId: commit.objectId,
+    },
+    commit,
+    {
+      upsert: true,
+    }
+  );
+}
+
+module.exports = { populateCommits, getAllCommits, exitsCommitWithSha };
